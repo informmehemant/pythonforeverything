517fdf70e56870dc73074d1086231a0cd2aa7b8b
hemant.singh@standardbank.co.za <hemant.singh@standardbank.co.za>
2023-03-29 13:05:56 +0530

    adding singleton soln 4

--- design_pattern/singleton/singleton_3.py
+++ design_pattern/singleton/singleton_3.py
@@ -1,24 +1,36 @@
 '''
 Pattern name - SingleTon
 Pattern type - Creational Design Pattern
-
 '''
+'''
+some example using __call__ method
+'''
+class Test:
+    
+    def __init__(self):
+        self.x = 2
+    def __call__(self, y):
+        return self.x + y
+    
+call = Test()
 
+print(call(3))
+
+        
 # Solution - 3
-
-class SingleTonDecorator(object):
+class SingleTonDecorator:
     def __init__(self, klass):
         self.klass = klass
         self.instance = None
 
-    def __call__(self, *args,**kwargs):
+    def __call__(self, *args, **kwargs):
         if self.instance == None:
             self.instance = self.klass(*args, **kwargs)
         return self.instance
     
 
 @SingleTonDecorator
-class Logger(object):
+class Logger:
     def __init__(self):
         self.start = None
     def write(self, message):
@@ -26,5 +38,17 @@ class Logger(object):
             print(self.start, message)
         else:
             print(message)
-        
+
+
+logger1 = Logger()
+logger1.start = "# >"
+print("Logger 1", logger1)
+logger1.write("Logger1 object is created.")
+
+logger2 = Logger()
+logger2.start = "$ >"
+print("Logger 2", logger2)
+logger1.write("Logger2 object is created.")
+
+
 
